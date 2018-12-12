5da73a0808cc32b13607822968e04a7d7ff706c9
liuying <610618673@qq.com>
2018-12-12 10:29:18 +0800

    new version

--- datasource.py
+++ datasource.py
@@ -8,9 +8,6 @@ class Mnist():
     BATCH_SIZE = 100
 
 
-
-
-
     def __init__(self):
         # use numpy
         path_np = './mnist.npz'
@@ -28,7 +25,85 @@ class Mnist():
         self.train_loader = torch.utils.data.DataLoader(dataset=self.train_data, batch_size=Mnist.BATCH_SIZE, shuffle=True)
         self.test_loader = torch.utils.data.DataLoader(dataset=self.test_data, batch_size=Mnist.BATCH_SIZE, shuffle=True)
 
+    def mnsit_noniid(dataset, num_users):
+        """
+        Sample non-I.I.D client data from MNIST dataset
+        :param dataset:
+        :param num_users:
+        :return:
+        """
+        num_shards, num_imgs = 200, 300
+        # num_shards is 200, and idx_shard is 0-199
+        idx_shard = [i for i in range(num_shards)]
+        # print(idx_shard)
+        # dict_users is range in num_users  default is 100   type is dictionary
+        # 100 ge np array
+        dict_users = {i: np.array([]) for i in range(num_users)}
+        # print(len(dict_users),dict_users)
+        # idxs = 60000, 60000 is devide into 200 parts, and 300 images each part
+        # idxs = 1-59999
+        idxs = np.arange(num_shards * num_imgs)
+        # print(idxs)
+
+        # labels is dataset's label
+        # len is 60000
+        # it is real label
+        labels = dataset.train_labels.numpy()
+        # print(len(labels))
+        # for i in labels:
+        #     print(labels[i])
+
+        # sort labels
+        # idxs_labels is the match between 0-59999 and the real labels of dataset
+        idxs_labels = np.vstack((idxs, labels))
+        # print(idxs_labels)
+
+        # change into arrange depend on the arrange of labels
+        idxs_labels = idxs_labels[:, idxs_labels[1, :].argsort()]
+        # print(idxs_labels)
+
+        # finally the idxs arrange to labels
+        # new ids
+        # [30207  5662 55366 ... 23285 15728 11924]
+        idxs = idxs_labels[0, :]
+        # print(idxs)
+
+        print(idx_shard)
+        # divide and assign
+        # 100 ge users
+        for i in range(num_users):
+            # select 2 parts from dataset for every num_users
+            # use p a2 = np.random.choice(a=5, size=3, replace=False, p=[0.2, 0.1, 0.3, 0.4, 0.0])
+            rand_set = set(np.random.choice(idx_shard, 2, replace=False))
+            # print(rand_set)
+            idx_shard = list(set(idx_shard) - rand_set)
+            print(idx_shard)
+            for rand in rand_set:
+                dict_users[i] = np.concatenate((dict_users[i], idxs[rand * num_imgs:(rand + 1) * num_imgs]), axis=0)
+        # print(len(dict_users[0]))
+        # finally return each user have which 600 images ,every user have a array contain 600 ge number
+        return dict_users
+
+    def mnist_iid(dataset, num_users):
+        """
+        Sample I.I.D. client data from MNIST dataset
+        :param dataset:
+        :param num_users:
+        :return: dict of image index
+        """
+        num_items = int(len(dataset) / num_users)
+        dict_users, all_idxs = {}, [i for i in range(len(dataset))]
+        for i in range(num_users):
+            dict_users[i] = set(np.random.choice(all_idxs, num_items, replace=False))
+            all_idxs = list(set(all_idxs) - dict_users[i])
+        return dict_users
+
     def get_train_data(self):
+
+
         return self.train_loader
     def get_test_data(self):
+
+
+
         return self.test_data
--- slave.py
+++ slave.py
@@ -1,4 +1,5 @@
 import torch
+import torch.distributed.deprecated as dist
 from datasource import Mnist
 import model
 import time
@@ -8,65 +9,70 @@ from torch.multiprocessing import Process
 MAX_EPOCH = 50
 LR = 0.001
 
+
 def get_new_model(model):
     for param in model.parameters():
-        torch.distributed.recv(param.data, src=0)
-    #print(torch.distributed.get_rank())
-    return  model
+        dist.recv(param.data, src=0)
+    # print(dist.get_rank())
+    return model
+
 
 def run(size, rank):
-
     modell = model.CNN()
+    # modell = model.AlexNet()
+
     optimizer = torch.optim.Adam(modell.parameters(), lr=LR)
     loss_func = torch.nn.CrossEntropyLoss()
 
-    #size = torch.distributed.get_world_size()
-    #rank = torch.distributed.get_rank()
+    # size = dist.get_world_size()
+    # rank = dist.get_rank()
 
     train_loader = Mnist().get_train_data()
 
     test_data = Mnist().get_test_data()
-    test_x = torch.unsqueeze(test_data.test_data, dim=1).type(torch.FloatTensor)[:2000]/255.   # shape from (2000, 28, 28) to (2000, 1, 28, 28), value in range(0,1)
-    test_y = test_data.test_labels[:2000]
+    test_x = torch.unsqueeze(test_data.test_data, dim=1).type(
+        torch.FloatTensor) / 255.  # shape from (2000, 28, 28) to (2000, 1, 28, 28), value in range(0,1)
+    test_y = test_data.test_labels
 
     group_list = []
     for i in range(size):
         group_list.append(i)
-    group = torch.distributed.new_group(group_list)
-
+    group = dist.new_group(group_list)
 
     for epoch in range(MAX_EPOCH):
 
         modell = get_new_model(modell)
-        current_model = copy.deepcopy(modell)
+        # current_model = copy.deepcopy(modell)
 
         for step, (b_x, b_y) in enumerate(train_loader):
-
-            #modell = get_new_model(modell)
-            #current_model = copy.deepcopy(modell)
+            # modell = get_new_model(modell)
+            # current_model = copy.deepcopy(modell)
 
             output = modell(b_x)[0]
             loss = loss_func(output, b_y)
             optimizer.zero_grad()
-            loss.backward()   
+            loss.backward()
             optimizer.step()
 
+        # new_model = copy.deepcopy(modell)
 
-        new_model = copy.deepcopy(modell)
+        # for param1, param2 in zip( current_model.parameters(), new_model.parameters() ):
+        # dist.reduce(param2.data-param1.data, dst=0, op=dist.reduce_op.SUM, group=group)
 
-        for param1, param2 in zip( current_model.parameters(), new_model.parameters() ):
-            torch.distributed.reduce(param2.data-param1.data, dst=0, op=torch.distributed.reduce_op.SUM, group=group)
-
+        for param in modell.parameters():
+            dist.reduce(param, dst=0, op=dist.reduce_op.SUM, group=group)
 
         test_output, last_layer = modell(test_x)
         pred_y = torch.max(test_output, 1)[1].data.numpy()
         accuracy = float((pred_y == test_y.data.numpy()).astype(int).sum()) / float(test_y.size(0))
-        print('Epoch: ', epoch, ' Rank: ', rank, '| train loss: %.4f' % loss.data.numpy(), '| test accuracy: %.2f' % accuracy)
+        print('Epoch: ', epoch, ' Rank: ', rank, '| train loss: %.4f' % loss.data.numpy(),
+              '| test accuracy: %.2f' % accuracy)
 
-                
+
 def init_processes(size, rank, run):
-    torch.distributed.init_process_group(backend='tcp', init_method='tcp://127.0.0.1:5000', world_size=size, rank=rank)
+    dist.init_process_group(backend='tcp', init_method='tcp://127.0.0.1:5000', world_size=size, rank=rank)
     run(size, rank)
+
 
 if __name__ == "__main__":
     size = 4
@@ -78,4 +84,3 @@ if __name__ == "__main__":
 
     for p in processes:
         p.join()
-    
